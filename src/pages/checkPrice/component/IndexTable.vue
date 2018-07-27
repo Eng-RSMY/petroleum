37a7988771518df1ba03567381d9d7c66547e5de
3993010@qq.com <Aa12344321>
2018-07-27 15:46:39 +0800

    价格查询

---
+++ src/pages/checkPrice/component/IndexTable.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="table">
+      <ul v-for="(item,index) in tableList" :key="index">
+        <li>{{item.serialNumber}}</li>
+        <li>{{item.category}}</li>
+        <li>{{item.type}}</li>
+        <li>{{item.price}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "IndexTitle",
+    data () {
+      return {
+        tableList:[{
+          serialNumber: '1',
+          category: '97#',
+          type: '92#',
+          price:'100'
+          },
+          {
+            serialNumber: '1',
+            category: '97#',
+            type: '92#',
+            price:'100'
+          },
+          {
+            serialNumber: '1',
+            category: '97#',
+            type: '92#',
+            price:'100'
+          },
+          {
+            serialNumber: '1',
+            category: '97#',
+            type: '92#',
+            price:'100'
+          },
+          {
+            serialNumber: '1',
+            category: '97#',
+            type: '92#',
+            price:'100'
+          },
+          {
+            serialNumber: '1',
+            category: '97#',
+            type: '92#',
+            price:'100'
+          },]
+      }
+    },
+  };
+</script>
+
+<style scoped>
+  .table {
+    width: 100%;
+    height: 35px;
+  }
+  .table ul {
+    display: flex;
+    font-size: 16px;
+    background: #fff;
+  }
+  .table ul:nth-child(2n){
+    background: #F6F6F6;
+  }
+  .table ul li {
+    width: 25%;
+    height: 35px;
+    line-height: 35px;
+    text-align: center;
+    color: black;
+  }
+  .table ul li:nth-child(-n+3) {
+    border-right: 1px solid #ccc;
+  }
+</style>
---
+++ src/pages/checkPrice/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <index-title></index-title>
+      <index-table></index-table>
+    </div>
+</template>
+
+<script>
+  // 标题
+  import IndexTitle from './component/IndexTiTle'
+  // 列表
+  import IndexTable from './component/IndexTable'
+  export default {
+    name: "index",
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      IndexTitle,
+      IndexTable
+    }
+  };
+</script>
+<style scoped>
+
+</style>
